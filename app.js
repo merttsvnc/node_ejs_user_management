3e5251dc6e8b98ff7e0ce8493368402138aafffc
merttsvnc <merttsvnc@gmail.com>
2023-10-04 22:10:46 +0900

    main page and header partials added

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-require("dotenv").config()
-const express = require("express")
-const expressLayout = require("express-ejs-layouts")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const app = express();
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Static file
-app.use(express.static("public") )
+app.use(express.static("public"));
 
 // Template engine
-app.use(expressLayout)
-app.set("layout", "./layouts/main")
-app.set("view engine", "ejs")
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  const locals = {
+    title: "nodejs",
+    description: "Free NodeJs User Management system",
+  };
+
+  res.render("index", locals);
+});
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     // await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`Server is listening on port ${port}...`))
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-start()
+start();
